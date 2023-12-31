f8f2232acc7be412a6f8794d68b7c426cf071df2
Praful Mali <76441868+PrafulMali20@users.noreply.github.com>
2023-12-31 10:24:52 +0530

    chip & dip

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:ChipNDip@gmail.com">ChipNDip@gmail.com</a>
+         <a href="mailto:ChipNDipWork@gmail.com.com">ChipNDipWork@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10:00am to 11:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Hiran Magri, Udaipur</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="MobNo:1234567890">1234567890</a>
+         <a href="MobNo:1112223333">1112223333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">Thank You To Visit in Chip N Dip Since @ <?= date('2015'); ?> by <span>mr. Mayur Chodhary</span> |visit Again</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
